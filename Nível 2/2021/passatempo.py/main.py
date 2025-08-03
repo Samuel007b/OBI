1dc2b129425d00b4ff907b7b2fc6d5bfe3264851
Samuel Miranda <smc14@aluno.ifnmg.edu.br>
2025-08-03 14:50:19 -0300

    Create main.py

---
+++ Nível 2/2021/passatempo.py/main.py
@@ -0,0 +1,19 @@
+L = int(input())
+C = int(input())
+variavel = []
+Sx = [0]*L
+Sy = [0]*C
+for i in range(0, L):
+    for j in range(0, C):
+        string = input()
+        variavel.append(string)
+    Sx[i] = int(input())
+for i in range(0,C):
+    Sy[i] = int(input())
+
+
+
+
+
+
+variaveis = sorted(variavel)
